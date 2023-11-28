9bb97faeb259f6ffc9e9c5c7e36051fbdc56f99d
nilayb12 <65295315+nilayb12@users.noreply.github.com>
2023-11-28 16:12:49 +0530

    Update Dependencies

--- index.php
+++ index.php
@@ -7,18 +7,18 @@
     <title>CSV Viewer</title>
     <link rel="stylesheet" href="style.css">
     <link href="https://code.jquery.com/ui/1.13.2/themes/dot-luv/jquery-ui.min.css" rel="stylesheet">
-    <link href="https://cdn.datatables.net/1.13.6/css/dataTables.jqueryui.min.css" rel="stylesheet">
+    <link href="https://cdn.datatables.net/1.13.8/css/dataTables.jqueryui.min.css" rel="stylesheet">
     <link href="https://cdn.datatables.net/autofill/2.6.0/css/autoFill.jqueryui.css" rel="stylesheet">
     <link href="https://cdn.datatables.net/buttons/2.4.2/css/buttons.jqueryui.min.css" rel="stylesheet">
     <link href="https://cdn.datatables.net/colreorder/1.7.0/css/colReorder.jqueryui.min.css" rel="stylesheet">
     <link href="https://cdn.datatables.net/datetime/1.5.1/css/dataTables.dateTime.min.css" rel="stylesheet">
     <link href="https://cdn.datatables.net/fixedcolumns/4.3.0/css/fixedColumns.jqueryui.min.css" rel="stylesheet">
     <link href="https://cdn.datatables.net/fixedheader/3.4.0/css/fixedHeader.jqueryui.min.css" rel="stylesheet">
-    <link href="https://cdn.datatables.net/keytable/2.10.0/css/keyTable.jqueryui.min.css" rel="stylesheet">
+    <link href="https://cdn.datatables.net/keytable/2.11.0/css/keyTable.jqueryui.min.css" rel="stylesheet">
     <link href="https://cdn.datatables.net/responsive/2.5.0/css/responsive.jqueryui.min.css" rel="stylesheet">
-    <link href="https://cdn.datatables.net/rowgroup/1.4.0/css/rowGroup.jqueryui.min.css" rel="stylesheet">
+    <link href="https://cdn.datatables.net/rowgroup/1.4.1/css/rowGroup.jqueryui.min.css" rel="stylesheet">
     <link href="https://cdn.datatables.net/rowreorder/1.4.1/css/rowReorder.jqueryui.min.css" rel="stylesheet">
-    <link href="https://cdn.datatables.net/scroller/2.2.0/css/scroller.jqueryui.min.css" rel="stylesheet">
+    <link href="https://cdn.datatables.net/scroller/2.3.0/css/scroller.jqueryui.min.css" rel="stylesheet">
     <link href="https://cdn.datatables.net/searchbuilder/1.6.0/css/searchBuilder.jqueryui.min.css" rel="stylesheet">
     <link href="https://cdn.datatables.net/searchpanes/2.2.0/css/searchPanes.jqueryui.min.css" rel="stylesheet">
     <link href="https://cdn.datatables.net/select/1.7.0/css/select.jqueryui.min.css" rel="stylesheet">
@@ -50,8 +50,8 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
-    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/1.13.6/js/dataTables.jqueryui.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.8/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.8/js/dataTables.jqueryui.min.js"></script>
     <script src="https://cdn.datatables.net/autofill/2.6.0/js/dataTables.autoFill.min.js"></script>
     <script src="https://cdn.datatables.net/autofill/2.6.0/js/autoFill.jqueryui.min.js"></script>
     <script src="https://cdn.datatables.net/buttons/2.4.2/js/dataTables.buttons.min.js"></script>
@@ -63,12 +63,12 @@
     <script src="https://cdn.datatables.net/datetime/1.5.1/js/dataTables.dateTime.min.js"></script>
     <script src="https://cdn.datatables.net/fixedcolumns/4.3.0/js/dataTables.fixedColumns.min.js"></script>
     <script src="https://cdn.datatables.net/fixedheader/3.4.0/js/dataTables.fixedHeader.min.js"></script>
-    <script src="https://cdn.datatables.net/keytable/2.10.0/js/dataTables.keyTable.min.js"></script>
+    <script src="https://cdn.datatables.net/keytable/2.11.0/js/dataTables.keyTable.min.js"></script>
     <script src="https://cdn.datatables.net/responsive/2.5.0/js/dataTables.responsive.min.js"></script>
     <script src="https://cdn.datatables.net/responsive/2.5.0/js/responsive.jqueryui.js"></script>
-    <script src="https://cdn.datatables.net/rowgroup/1.4.0/js/dataTables.rowGroup.min.js"></script>
+    <script src="https://cdn.datatables.net/rowgroup/1.4.1/js/dataTables.rowGroup.min.js"></script>
     <script src="https://cdn.datatables.net/rowreorder/1.4.1/js/dataTables.rowReorder.min.js"></script>
-    <script src="https://cdn.datatables.net/scroller/2.2.0/js/dataTables.scroller.min.js"></script>
+    <script src="https://cdn.datatables.net/scroller/2.3.0/js/dataTables.scroller.min.js"></script>
     <script src="https://cdn.datatables.net/searchbuilder/1.6.0/js/dataTables.searchBuilder.min.js"></script>
     <script src="https://cdn.datatables.net/searchbuilder/1.6.0/js/searchBuilder.jqueryui.min.js"></script>
     <script src="https://cdn.datatables.net/searchpanes/2.2.0/js/dataTables.searchPanes.min.js"></script>
