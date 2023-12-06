326aad8ede5521da283589f566123d0e9948f402
Nilay Baranwal <65295315+nilayb12@users.noreply.github.com>
2023-12-06 18:07:12 +0530

    CSV Icon on Download Button

--- viewTable.js
+++ viewTable.js
@@ -109,10 +109,14 @@ function initDataTable() {
         // responsive: true,
         deferRender: true,
         buttons: [
-            'colvis',
+            {
+                extend: 'colvis',
+                // collectionTitle: 'Column Visibility Panel'
+            },
             {
                 extend: 'csv',
-                text: 'Download CSV',
+                text: '<i class="fa-solid fa-file-csv" />',
+                titleAttr: 'Download Data as CSV',
                 exportOptions: {
                     columns: ':visible'
                 }
@@ -133,7 +137,7 @@ function initDataTable() {
                         });
                 });
             $('#tableData_filter input')
-                .after('<span class="fa-icon"><i class="fa-solid fa-magnifying-glass"></i></span>');
+                .after('<span class="fa-icon" /><i class="fa-solid fa-magnifying-glass" />');
         },
         columnDefs: [
             {
